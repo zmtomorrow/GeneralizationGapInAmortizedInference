14c3570b0da23c2e2b761a86a27e24752d161796
Mingtian Zhang <zmt534885880@gmail.com>
2022-05-18 10:28:25 +0100

    Update run.py

--- run.py
+++ run.py
@@ -1,9 +1,13 @@
 import torch
-from train import *
+from torch import optim
+from utils import *
+from model import *
+import numpy as np
+from tqdm import tqdm
+import os
 
 
 opt = {}
-
 if torch.cuda.is_available():
     torch.backends.cudnn.deterministic = True
     torch.backends.cudnn.benchmark = True
@@ -13,39 +17,76 @@ else:
     opt["device"] = torch.device("cpu")
     opt["if_cuda"] = False
 
-#opt['data_set']='MNIST'
-#opt['x_dis']='Logistic'
+opt['data_set']='CIFAR'
+opt['x_dis']='Logistic' ## or MixLogistic 
+opt['z_channels']=16
+opt['z_dim']=opt['z_channels']*64
 
-opt['data_set']='BinaryMNIST'
-opt['x_dis']='Bernoulli'
-
+opt['epochs'] = 1000
 opt['dataset_path']='../data/'
-opt['save_path']='./save/'
-opt['result_path']='./results/'
-opt['epochs'] = 200
+opt['save_path']='./'+opt['data_set']+'/'+opt['x_dis']+'_z'+str(opt['z_dim'])+'/'
+if not os.path.exists(opt['save_path']):
+    os.makedirs(opt['save_path'])
+opt['result_path']=opt['save_path']
 opt['batch_size'] = 100
-opt['test_batch_size']=1000
+opt['test_batch_size']=200
 opt['if_regularizer']=False
 opt['load_model']=False
-opt['lr']=1e-3
-opt['z_dim']=10
-opt['SelfConsistPhase']=False
-opt['if_bn']=True
-opt['h_layer_num']=2
+opt['lr']=1e-4
+opt['data_aug']=False
+opt["seed"]=0
+opt['if_save']=True
+opt['save_epoch']=50
+opt['additional_epochs']=100
+opt['sample_size']=100
+opt['if_save']=True
 
 
-JointELBO_train=[]
-JointELBO_test=[]
-PartialEM_train=[]
-PartialEM_test=[]
+np.random.seed(opt['seed'])
+torch.manual_seed(opt['seed'])
 
-for opt["seed"] in range(0,5):
-    train_BPD, test_BPD = JointELBOTrain(opt)
-    JointELBO_train.append(train_BPD)
-    JointELBO_test.append(test_BPD)
+train_data,test_data,train_data_evaluation=LoadData(opt)
+model=VAE(opt).to(opt['device'])
 
-    np.save(opt['result_path']+'JointELBO_train', JointELBO_train)
-    np.save(opt['result_path']+'JointELBO_test', JointELBO_test)
-    
+# if opt['load_model']==True:
+#     model.load_state_dict(torch.load(opt['save_path']+opt['load_name']))
+
+optimizer = optim.Adam(model.parameters(), lr=opt['lr'])
 
 
+test_BPD_list=[]
+train_BPD_list=[]
+
+for epoch in range(1, opt['epochs'] + 1):
+    model.train()
+    for x, _ in tqdm(train_data):
+        optimizer.zero_grad()
+        L = -model(x.to(opt['device']))
+        L.backward()
+        optimizer.step()
+
+    model.eval()
+    with torch.no_grad():
+        train_BPD=0.
+        for x, _ in train_data_evaluation:
+            train_BPD+=-model(x.to(opt['device'])).item()
+        train_BPD = train_BPD/(len(train_data_evaluation)*np.prod(x.size()[-3:]))
+        print('epoch:',epoch,'train_BPD:',train_BPD)
+
+
+        test_BPD=0.
+        for x, _ in test_data:
+            test_BPD+=-model(x.to(opt['device'])).item()
+        test_BPD = test_BPD/(len(test_data)*np.prod(x.size()[-3:]))
+        print('epoch:',epoch,'test_BPD:',test_BPD)
+        
+        
+     
+        test_BPD_list.append(test_BPD)
+        train_BPD_list.append(train_BPD)
+
+        np.save(opt['result_path']+'testBPD',test_BPD_list)
+        np.save(opt['result_path']+'trainlBPD',train_BPD_list)
+        
+        if epoch%100==0:
+            torch.save(model.state_dict(),opt['save_path']+'epoch'+str(epoch)+'.pth')
