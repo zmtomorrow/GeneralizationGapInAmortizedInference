bd58c21d80d5a997a628d164df66bef9e3aca976
Mingtian Zhang <zmt534885880@gmail.com>
2022-05-22 13:59:39 +0100

    Update model.py

--- model.py
+++ model.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 from network import *
 from distributions import *
-from tools import *
+from utils import  batch_KL_diag_gaussian_std
 from torch.distributions.bernoulli import Bernoulli
 import numpy as np
 import torch.nn.functional as F
@@ -14,7 +14,6 @@ class VAE(nn.Module):
         self.device=opt['device']
         if opt['data_set']=='BinaryMNIST':
             self.x_flat_dim=784
-            self.o_encoder=densenet_encoder(input_dim=self.x_flat_dim, h_layer_num=opt['h_layer_num'], z_dim=self.z_dim,if_bn=opt['if_bn'])
             self.encoder=densenet_encoder(input_dim=self.x_flat_dim, h_layer_num=opt['h_layer_num'], z_dim=self.z_dim,if_bn=opt['if_bn'])
             self.decoder=densenet_decoder(o_dim=1, h_layer_num=opt['h_layer_num'], z_dim=self.z_dim,if_bn=opt['if_bn'])
             self.criterion  = lambda  data,params : Bernoulli(logits=params).log_prob(data).sum([1,2,3])
@@ -23,13 +22,15 @@ class VAE(nn.Module):
             self.x_flat_dim=784
             if opt['x_dis']=='Logistic':
                 self.out_channels=2
-                self.o_encoder=densenet_encoder(input_dim=self.x_flat_dim, h_layer_num=opt['h_layer_num'], z_dim=self.z_dim,if_bn=opt['if_bn'])
                 self.encoder=densenet_encoder(input_dim=self.x_flat_dim, h_layer_num=opt['h_layer_num'], z_dim=self.z_dim,if_bn=opt['if_bn'])
                 self.decoder=densenet_decoder(o_dim=2, h_layer_num=opt['h_layer_num'], z_dim=self.z_dim,if_bn=opt['if_bn'])
-                self.criterion  = lambda  data,params : batch_logistic_logp(params[:,0:1,:,:],params[:,1:2,:,:],data)
-                self.sample_op = lambda params: batch_logistic_sample(params[:,0:1,:,:],params[:,1:2,:,:])
+                self.criterion  = lambda  data,params : discretized_logistic(params[:,0:1,:,:],params[:,1:2,:,:],data)
+                self.sample_op = lambda params: discretized_logistic_sample(params[:,0:1,:,:],params[:,1:2,:,:])
+            else: 
+                raise NotImplementedError
         
         elif opt['data_set'] in ['CIFAR','SVHN']:
+            self.encoder=fc_encoder(latent_channels=opt['z_channels'])
             if opt['x_dis']=='MixLogistic':
                 self.decoder=fc_decoder(latent_channels=opt['z_channels'],out_channels=100)       
                 self.criterion  = lambda  data,params :discretized_mix_logistic_uniform(data, params)
@@ -44,11 +45,8 @@ class VAE(nn.Module):
         self.params = list(self.parameters())
 
 
-    def forward(self, x, optimal=False):
-        if optimal:
-            z_mu, z_std = self.o_encoder(x)
-        else:
-            z_mu, z_std = self.encoder(x)
+    def forward(self, x):
+        z_mu, z_std = self.encoder(x)
         eps = torch.randn_like(z_mu).to(self.device)
         zs = eps.mul(z_std).add_(z_mu)
         pxz_params = self.decoder(zs)
